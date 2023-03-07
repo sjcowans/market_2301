a0aa591e058b41c12739f568ed616e83283a848a
Sean Cowans <zedesia@yahoo.com>
2023-03-07 10:58:29 -0700

    test: Add tests to market spec

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -103,4 +103,77 @@ describe Market do
     expect(vendor2.potential_revenue).to eq(345.00)
     expect(vendor3.potential_revenue).to eq(48.75)
   end
+
+  it 'can create #sorted_item_list' do
+    market = Market.new("South Pearl Street Farmers Market")    
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)    
+    vendor1.stock(item2, 7)    
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")    
+    vendor2.stock(item4, 50)    
+    vendor2.stock(item3, 25)
+    vendor3 = Vendor.new("Palisade Peach Shack")    
+    vendor3.stock(item1, 65)  
+    market.add_vendor(vendor1)    
+    market.add_vendor(vendor2)    
+    market.add_vendor(vendor3)
+
+    expect(market.sorted_item_list).to eq(["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"])
+  end
+
+  it 'can show #total_inventory' do
+  market = Market.new("South Pearl Street Farmers Market")    
+  vendor1 = Vendor.new("Rocky Mountain Fresh")
+  item1 = Item.new({name: 'Peach', price: "$0.75"})
+  item2 = Item.new({name: 'Tomato', price: "$0.50"})
+  item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+  item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+  vendor1.stock(item1, 35)    
+  vendor1.stock(item2, 7)    
+  vendor2 = Vendor.new("Ba-Nom-a-Nom")    
+  vendor2.stock(item4, 50)    
+  vendor2.stock(item3, 25)
+  vendor3 = Vendor.new("Palisade Peach Shack")    
+  vendor3.stock(item1, 65)  
+  market.add_vendor(vendor1)    
+  market.add_vendor(vendor2)    
+  market.add_vendor(vendor3)
+
+  expect(market.total_inventory).to eq(
+  {
+  "Banana Nice Cream"=>{"quantity"=>50, "vendors"=>["Ba-Nom-a-Nom"]},
+  "Peach"=>{"quantity"=>100, "vendors"=>["Rocky Mountain Fresh", "Palisade Peach Shack"]},
+  "Peach-Raspberry Nice Cream"=>{"quantity"=>25, "vendors"=>["Ba-Nom-a-Nom"]},
+  "Tomato"=>{"quantity"=>7, "vendors"=>["Rocky Mountain Fresh"]}
+  })
+  end
+
+  it 'can list #overpriced_items' do
+  market = Market.new("South Pearl Street Farmers Market")    
+  vendor1 = Vendor.new("Rocky Mountain Fresh")
+  item1 = Item.new({name: 'Peach', price: "$0.75"})
+  item2 = Item.new({name: 'Tomato', price: "$0.50"})
+  item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+  item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+  vendor1.stock(item1, 35)    
+  vendor1.stock(item2, 7)    
+  vendor2 = Vendor.new("Ba-Nom-a-Nom")    
+  vendor2.stock(item4, 50)    
+  vendor2.stock(item3, 25)
+  vendor3 = Vendor.new("Palisade Peach Shack")    
+  vendor3.stock(item1, 65)  
+  market.add_vendor(vendor1)    
+  market.add_vendor(vendor2)    
+  market.add_vendor(vendor3)
+
+  expect(market.overstocked_items).to eq("Peach")
+
+  vendor2.stock(item2, 50)
+
+  expect(market.overstocked_items).to eq("Peach, Tomato")
+  end
 end
