d87473a59e0c1d0ae2ef747c7b12a4d9156aa002
Sean Cowans <zedesia@yahoo.com>
2023-03-07 09:45:20 -0700

    feat: Create vendor class

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,18 @@
 class Vendor
-  
+  attr_reader :name, :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    value = 0
+    @inventory.each { |k,v| if k == item then value = v end }
+    value
+  end
+
+  def stock(item, number)
+    @inventory[item] += number
+  end
 end
