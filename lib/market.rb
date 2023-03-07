d6958db3707e33d9031cb0cf69f987aea38f2b4a
Sean Cowans <zedesia@yahoo.com>
2023-03-07 10:07:44 -0700

    feat: Create market class

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,26 @@
 class Market
-  
+  attr_reader :name, :vendors, :vendor_names
+
+  def initialize(name)
+    @name = name
+    @vendors = []
+    @vendor_names = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+    @vendor_names << vendor.name
+  end
+
+  def vendors_that_sell(item)
+    vendors = []
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item1, amount|
+        if item1 == item
+          vendors << vendor
+        end
+      end
+    end
+    vendors
+  end
 end
